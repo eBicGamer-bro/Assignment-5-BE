2e0ac3721b80643ca48bd8ed29f0c2231d16913c
Fares Fouad <faresfouadhussein90@gmail.com>
2026-02-19 17:00:21 +0200

    Add files via upload

---
+++ Assignment 5/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Assignment_5
+{
+    class MyCookieCollection
+    {
+        string username,theme,language;
+        public MyCookieCollection()
+        {
+           username = string.Empty;
+           theme = string.Empty;
+           language = string.Empty;
+        }
+        public string this[string key]
+        {
+            get
+            {
+                if (key.Equals("username"))
+                    return username;
+                else if (key.Equals("theme"))
+                    return theme;
+                else if (key.Equals("language"))
+                    return language;
+                else
+                    return null;
+            }
+            set
+            {
+                if (key.Equals("username"))
+                    username = value;
+                else if (key.Equals("theme"))
+                    theme = value;
+                else if (key.Equals("language"))
+                    language = value;
+            }
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MyCookieCollection cookies = new MyCookieCollection();
+            cookies["username"] = "John Doe";
+            cookies["theme"] = "dark";
+            cookies["language"] = "en";
+            Console.WriteLine("Username: " + cookies["username"]);
+            Console.WriteLine("Theme: " + cookies["theme"]);
+            Console.WriteLine("Language: " + cookies["language"]);
+
+        }
+    }
+}

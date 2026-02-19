a56bfa899a3b60dcbb66c073888b80b60582f033
Fares Fouad <faresfouadhussein90@gmail.com>
2026-02-19 16:48:30 +0200

    Add files via upload

---
+++ Assignment 5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_5
+{
+    class Grades
+    {
+        public int[] grades;
+        public Grades()
+        {
+            grades = new int[5];
+        }
+        public int this[int index]
+        {
+            get { return grades[index]; }
+            set { grades[index] = value; }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Grades grades = new Grades();
+            grades[0] = 95;
+            grades[1] = 88;
+            Console.WriteLine(grades[0]);
+            Console.WriteLine(grades[1]);
+
+
+
+        }
+    }
+}
